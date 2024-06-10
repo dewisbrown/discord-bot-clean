4f33c5d8f8ad14ea9b8b1b4c8ccf8ac97515c278
dewisbrown <luismoreno2103@gmail.com>
2024-06-09 20:22:17 -0500

    removed function

--- project_utils/change_tables.py
+++ project_utils/change_tables.py
@@ -19,20 +19,6 @@ def add_column_to_table(table_name: str, col_name: str, col_type: str) -> None:
     cursor.close()
     conn.close()
 
-def remove_column_from_table(table_name: str, col_name: str) -> None:
-    """
-    Removes a column from an existing table.
-    """
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-
-    # TODO: implement logic for removing column
-    query = f''
-    cursor.execute(query)
-
-    conn.commit()
-    conn.close()
-
 def print_table_columns(table_name: str) -> list:
     """
     Prints table columns to console.

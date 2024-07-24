1a7fc072cec46c2f2000b44c5d9e0c1f282f1d53
dewisbrown <luismoreno2103@gmail.com>
2024-07-24 06:25:58 -0500

    moved functions to new script, moved DB_FILE to __init__

--- utils/change_tables.py
+++ utils/change_tables.py
@@ -1,10 +1,7 @@
 import sqlite3
-import os
-import datetime
 
+from . import DB_FILE
 
-rel_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'battlepass.db')
-DB_FILE = os.path.abspath(rel_path)
 
 def add_column_to_table(table_name: str, col_name: str, col_type: str) -> None:
     """
@@ -35,32 +32,4 @@ def print_table_columns(table_name: str) -> list:
         print(col)
 
     cursor.close()
-    conn.close()
-
-def set_daily_redemption() -> None:
-    """
-    Sets all users' 'daily_redemption' to 1 day ago.
-    """
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    query = 'UPDATE battlepass SET daily_redemption = ?'
-    cursor.execute(query, (yesterday,))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-def update_user_points(points: int, user_name: str) -> None:
-    """
-    Manually set points for a user.
-    """
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-
-    query = 'UPDATE battlepass SET points = ? WHERE user_name = ?'
-    cursor.execute(query, (points, user_name))
-
-    conn.commit()
     conn.close()

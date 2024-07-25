18ca760b85c372ec15b0570dc2e2baa1fbd773b3
Luis Moreno <luismoreno2103@gmail.com>
2024-07-25 17:54:23 -0500

    included commands.py import

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,7 @@ import os
 from .change_tables import add_column_to_table, print_table_columns
 from .modify_db import set_daily_redemption, update_user_points, create_battlepass_entry
 from .misc import decimal_to_hex
+from .commands import calculate_points, points_to_level_up, get_command_help
 from .db_interface import *
 
 

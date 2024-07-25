c560ccee820247bbe47afcbd6a300b7615ca9b0d
dewisbrown <luismoreno2103@gmail.com>
2024-07-25 07:37:08 -0500

    included db_interface import

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from .change_tables import add_column_to_table, print_table_columns
 from .modify_db import set_daily_redemption, update_user_points, create_battlepass_entry
+from .db_interface import *
 
 
 rel_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'battlepass.db')
